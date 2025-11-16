0f14328767c7492e4e981fb79563934983c075cf
Guilherme Cardoso <gcs200206@gmail.com>
2025-11-16 16:18:02 -0300

    rename controllers

--- src/main/java/com/global/solution/api/user/UserController.java
+++ src/main/java/com/global/solution/api/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
