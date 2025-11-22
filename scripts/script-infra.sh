cbc091bfe453e6128b1f4f3502d426268fc60a55
hassan <hassan.chahine@oneconsultoria.net>
2025-11-22 03:55:39 -0300

    change script infra

--- scripts/script-infra.sh
+++ scripts/script-infra.sh
@@ -1,7 +1,7 @@
 # Variáveis
 RESOURCE_GROUP="rg-global-solution-2025"
 LOCATION="eastus"
-ACR_NAME="acrglobalsolution$RANDOM"
+ACR_NAME="acrglobalsolutionRM555178"
 ACI_NAME="aci-global-solution"
 
 # 1. Criar Grupo de Recursos
@@ -12,13 +12,12 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 echo "Criando ACR: $ACR_NAME..."
 az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
 
-# 3. Criar o contexto do ACI para reservar o nome DNS
 echo "Infraestrutura de Registry criada."
 echo "O Azure Container Instance (ACI) será provisionado dinamicamente via Pipeline."
 
 # 4. Exibir credenciais
 echo "------------------------------------------------"
-echo "INFRAESTRUTURA CRIADA COM SUCESSO"
+echo "infraestrutura:"
 echo "Resource Group: $RESOURCE_GROUP"
 echo "ACR Login Server: $(az acr show --name $ACR_NAME --query loginServer --output tsv)"
 echo "ACR Username: $(az acr credential show --name $ACR_NAME --query username --output tsv)"
